4cd4c516784c5cee81a5b68f6f0ecc76dd556d13
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-02-26 15:26:30 -0300

    20

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import { prisma } from "./lib/prisma.js";
 import app from "./server.js";
 
-const PORT = 3000;
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
-} else if (!process.env.CAPTCHA_API_KEY) {
-  throw new Error("CAPTCHA_API_KEY is not defined");
-} else if (!process.env.CND_API_URL) {
-  throw new Error("CND_API_URL is not defined");
-}
+["DATABASE_URL", "CAPTCHA_API_KEY", "CND_API_URL", "PORT"].forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`${envVar} is not defined`);
+  }
+});
 
 try {
   await prisma.$connect();
   console.log("Database connected");
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  app.listen(parseInt(process.env.PORT!), "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
   });
 } catch (err) {
   console.error("Database connection failed");
   console.error(err);
   process.exit(1);
 }
-
